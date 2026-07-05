b3c7eae1b4ab9d787566ba94c33d34a80d4fa61b
dabanzall <dabantofiqhussein@gmail.com>
2026-07-05 01:37:00 -0700

    episode-11: postgresql and prisma setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const prisma = require('./prisma')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// Middleware
 app.use(cors())
 app.use(express.json())
 
-// Health check
-app.get('/', (req, res) => {
-  res.json({ message: 'ShopZone API is running!' })
+app.get('/', async (req, res) => {
+  try {
+    await prisma.$connect()
+    res.json({ message: 'ShopZone API is running! Database connected ✅' })
+  } catch (error) {
+    res.json({ message: 'Database connection failed ❌', error })
+  }
 })
-
-// Routes — coming soon
-// app.use('/api/products', productRoutes)
-// app.use('/api/auth', authRoutes)
-// app.use('/api/orders', orderRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
